5301979ba28137f84560d5bf96e0bdeb03fb4fa5
Sathish R <r.sathish@kgkite.ac.in>
2019-11-06 05:27:27 +0000

    UNit4 Material

---
+++ main.py
@@ -0,0 +1,18 @@
+alist=[] #empty list
+blist=[1,2,3] #elements in list
+clist=["a","b","c"] # string elements in list
+dlist=[1,2,3,6,1,1,2,2] #List can accept repeated elements
+elist=[1,"one",1.5,1+5j,True,[5,6,"aa"],(5,9,"lk"),{1,2,"a"},{1:96,"a":"aa"},range(3)] #List accept all types of elements
+print(alist)
+print(blist)
+print(clist)
+print(dlist)
+print(elist)
+#indexing
+print(clist[1])
+#mutability
+myfavcolors=["red","yellow","orange"]
+print("Before updation list elements",myfavcolors)
+#myfavcolors[3]=”white” -error because index range is only 2
+myfavcolors[2]="white"
+print("After updation list elements",myfavcolors)
